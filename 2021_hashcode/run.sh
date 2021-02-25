c52b3efc0dde5e947285ae7f3d1728b5e1bd971d
tabletenniser <tabletenniser@gmail.com>
2021-02-25 11:08:05 -0800

    Add format data

--- 2021_hashcode/run.sh
+++ 2021_hashcode/run.sh
@@ -1,4 +1,4 @@
-for file in *.in ; do 
+for file in *.txt ; do 
   echo $file
   /Users/zexuan/opt/anaconda3/bin/python s.py < $file > ${file%.in}.out
 done
--- 2021_hashcode/s.py
+++ 2021_hashcode/s.py
@@ -6,61 +6,62 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 class Solution(object):
-    def __init__(self, teams, pizzas):
-        self.ct_two, self.ct_three, self.ct_four = teams
-        self.pizzas = pizzas
-        self.pizzas.sort(key=lambda s: len(s[1])+random.random(), reverse=True)
+    def __init__(self, streets, intersections, vehicles):
+        self.streets = streets
+        self.vehicles = vehicles
+        self.intersections = intersections
+        eprint(streets)
+        eprint(intersections)
+        eprint(vehicles)
 
     def get_opt_solution(self):
         sol = []
-        score = 0
-        while True:
-            ing = set()
-            if len(self.pizzas) > 5 and self.ct_four > 0:
-                sol.append(self.pizzas[:4])
-                for p in self.pizzas[:4]:
-                    ing = ing.union(p[1])
-                score += len(ing) ** 2
-                self.ct_four -= 1
-                self.pizzas = self.pizzas[4:]
-            elif len(self.pizzas) >= 3 and self.ct_three > 0:
-                sol.append(self.pizzas[:3])
-                for p in self.pizzas[:3]:
-                    ing = ing.union(p[1])
-                score += len(ing) ** 2
-                self.ct_three -= 1
-                self.pizzas = self.pizzas[3:]
-            elif len(self.pizzas) >= 2 and self.ct_two > 0:
-                sol.append(self.pizzas[:2])
-                for p in self.pizzas[:2]:
-                    ing = ing.union(p[1])
-                score += len(ing) ** 2
-                self.ct_two -= 1
-                self.pizzas = self.pizzas[2:]
-            else:
-                break
-        return sol, score
+        for ind in intersections:
+            incoming_streets = intersections[ind]
+            sol.append((ind, [next(iter(incoming_streets)), 2]))
+        return sol
 
-teams = [int(s) for s in input().strip().split(" ")]
-pizza_cnt = teams[0]
-pizzas = []
-for index in range(pizza_cnt):
-    pizzas.append((index, set([s for s in input().split(" ")][1:])))
+D, I, S, V, F = [int(s) for s in input().strip().split(" ")]
+streets = {}
+intersections = {}
+for index in range(S):
+    B, E, name, length = [s for s in input().split(" ")]
+    inter = intersections.get(B, set())
+    inter.add(name)
+    intersections[B] = inter
+    inter = intersections.get(E, set())
+    inter.add(name)
+    intersections[E] = inter
+    streets[name] = ({B, E}, length)
 
-opt_sol = None
-max_score = -1
-i = 0 
-start = time.time()
-while time.time() - start < 300 and i < 500: # 2 minutes
-    s = Solution(teams[1:], pizzas)
-    sol, score=s.get_opt_solution()
-    if score > max_score:
-        eprint(i, score, time.time()-start)
-        max_score = score
-        opt_sol = sol
-    i += 1
+vehicles = []
+for index in range(V):
+    vehicles.append([s for s in input().split(" ")][1:])
 
+
+s = Solution(streets, intersections, vehicles)
+opt_sol=s.get_opt_solution()
+
+# opt_sol = None
+# max_score = -1
+# i = 0 
+# start = time.time()
+# while time.time() - start < 300 and i < 500: # 2 minutes
+#     s = Solution(teams[1:], pizzas)
+#     sol, score=s.get_opt_solution()
+#     if score > max_score:
+#         eprint(i, score, time.time()-start)
+#         max_score = score
+#         opt_sol = sol
+#     i += 1
+
+eprint("Solution:", opt_sol)
 print(len(opt_sol))
 for line in opt_sol:
+    print(line[0])
+    signals = line[1]
+    print(signals)
+    for sig in signals:
+        print(signals)
     orders = ' '.join([str(p[0]) for p in line])
     print(len(line), orders)
